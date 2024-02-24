9b8d17176df0bd26b62ebcb2d0ddf26fac12a05b
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-02-23 23:03:46 -0500

    Add Task store and drag and drop

--- src/components/ListTasks.tsx
+++ src/components/ListTasks.tsx
@@ -1,5 +1,9 @@
 import type { Task, TaskStatus } from "@/interfaces";
 import { TaskCard } from "./TaskCard";
+import { DragEvent, useState } from "react";
+import { useTaskStore } from "@/store";
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 interface Props {
   title: string;
@@ -7,11 +11,38 @@ interface Props {
   value?: TaskStatus;
 }
 
-export function ListTask({ title, tasks }: Props) {
+export function ListTask({ title, value, tasks }: Props) {
+  const isDragging = useTaskStore((s) => !!s.draggingTaskId);
+  const [onDragOver, setOnDragOver] = useState(false);
+
+  const handleDragOver = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setOnDragOver(true);
+  };
+  const handleDragLeave = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setOnDragOver(false);
+  };
+  const handleDrop = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setOnDragOver(false);
+  };
+
   return (
-    <div className="bg-two mx-4 rounded py-2 px-4">
-      <h2 className="font-semibold text-three select-none">{title}</h2>
-      <div>
+    <div
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+      className={twMerge(
+        clsx("bg-two mx-2 rounded-xl border-4 border-transparent", {
+          "border-gris border-dotted": isDragging,
+          "border-warning border-dotted": isDragging && onDragOver,
+        })
+      )}
+    >
+      <div className="py-2 px-2">
+        <h2 className="font-semibold text-three select-none">{title}</h2>
+
         {tasks.length > 0 ? (
           tasks.map((task) => <TaskCard key={task.id} task={task} />)
         ) : (
