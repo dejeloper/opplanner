c376d4b3528da68e0ba4a66b68ac5672f1a1adf3
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-03-07 18:38:10 -0500

    Update HeaderLogo component with new logo text

--- src/components/head/HeaderLogo.tsx
+++ src/components/head/HeaderLogo.tsx
@@ -3,7 +3,7 @@ import { IconMenu } from "../icons";
 export function HeaderLogo() {
   return (
     <div className="flex w-full">
-      <p className="px-3">Logo</p>
+      <p className="px-3">Opplanner</p>
       <IconMenu className="w-6 h-6" />
     </div>
   );
