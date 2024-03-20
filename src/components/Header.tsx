7890a1acfd918980262ad3dec693c0f81c7bbd98
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-03-20 13:55:29 -0500

    Refactor header component and add header dropdown menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
 import { HeaderLogo, HeaderNav } from "./head";
+import { HeaderMenu } from "./head/HeaderMenu";
 
 export function Header() {
   return (
     <section className="flex items-center border-b border-black/10 w-full h-[60px]">
-      <div className="container flex items-center px-3 mx-auto max-w-[1480px]">
-        <HeaderLogo />
-        <HeaderNav />
+      <div className="container flex justify-between items-center px-3 mx-auto max-w-[1480px]">
+        <div className="flex items-center">
+          <HeaderLogo />
+          <HeaderMenu />
+        </div>
+        <div className="flex w-72">
+          <HeaderNav />
+        </div>
       </div>
     </section>
   );
---
+++ src/components/head/HeaderDropdown.tsx
@@ -0,0 +1,62 @@
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+
+interface HeaderDropdownProps {
+  id: number;
+  name: string;
+  icon: string;
+  path: string;
+  active: boolean;
+}
+
+export const HeaderDropdown = ({
+  menu,
+  openDropdown,
+  nivel,
+}: {
+  menu: HeaderDropdownProps[];
+  openDropdown: boolean;
+  nivel: number;
+}) => {
+  let right = "";
+  switch (nivel) {
+    case 1:
+      right = "right-4";
+      break;
+    case 2:
+      right = "right-14";
+      break;
+    case 3:
+      right = "right-24";
+      break;
+    case 4:
+      right = "right-32";
+      break;
+    default:
+      right = "right-4";
+      break;
+  }
+
+  return (
+    <div
+      className={twMerge(
+        clsx(
+          right,
+          "top-8 left-0 absolute max-w-60 h-auto bg-white border border-slate-400 z-20 p-4 rounded-md shadow-md transition-all duration-300 ease-in-out overflow-hidden",
+          {
+            hidden: !openDropdown,
+            block: openDropdown,
+          }
+        )
+      )}
+    >
+      <ul>
+        {menu.map((item) => (
+          <li key={item.id}>
+            <a href={`${item.path}`}>{item.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
