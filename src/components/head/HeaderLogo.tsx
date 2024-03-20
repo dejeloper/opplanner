e1c671b5f7a67c5eeb9d59715a4ea4a7a5a430f7
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-03-20 13:55:00 -0500

    Refactor header navigation component

--- src/components/head/HeaderLogo.tsx
+++ src/components/head/HeaderLogo.tsx
@@ -1,17 +1,12 @@
-import { HeaderMenu } from "./HeaderMenu";
-
 export function HeaderLogo() {
   return (
-    <div className="flex items-center w-full">
-      <div className="px-3">
-        <img
-          className="w-10 h-10 border-2 border-white rounded-full"
-          src="/logo.svg"
-          alt="Logo Opplanner Dos aguacates"
-          title="Logo Opplanner"
-        />
-      </div>
-      <HeaderMenu />
+    <div className="px-3">
+      <img
+        className="w-10 h-10 border-2 border-white rounded-full"
+        src="/logo.svg"
+        alt="Logo Opplanner Dos aguacates"
+        title="Logo Opplanner"
+      />
     </div>
   );
 }
--- src/components/head/HeaderNav.tsx
+++ src/components/head/HeaderNav.tsx
@@ -1,21 +1,105 @@
+import clsx from "clsx";
+import { useState } from "react";
 import {
-  IconApps,
-  IconArrowDown,
-  IconNotification,
-  IconProfile,
-  IconSearch,
-} from "../icons";
+  RiApps2Fill,
+  RiArrowDownSLine,
+  RiNotification3Fill,
+  RiSearchLine,
+  RiUser3Fill,
+} from "react-icons/ri";
+import { twMerge } from "tailwind-merge";
+import { HeaderDropdown } from "./HeaderDropdown";
+import {
+  menuApps,
+  menuNotifications,
+  menuSearch,
+  menuUser,
+} from "../../data/dropdowns";
 
 export function HeaderNav() {
+  const [openDropdownUser, setOpenDropdownUser] = useState(false);
+  const [openDropdownNotifications, setOpenDropdownNotifications] =
+    useState(false);
+  const [openDropdownApps, setOpenDropdownApps] = useState(false);
+  const [openDropdownSearch, setOpenDropdownSearch] = useState(false);
+
   return (
-    <div className="navbar flex justify-center items-center gap-4">
-      <IconSearch className="w-8 h-8" />
-      <IconApps className="w-8 h-8" />
-      <IconNotification className="w-8 h-8" />
-      <div className="flex">
-        <IconProfile className="w-8 h-8" />
-        <IconArrowDown className="w-8 h-8" />
+    <div className="flex flex-col relative w-full">
+      <div
+        className={twMerge(
+          clsx("fixed top-0 left-0 w-full h-full bg-transparent z-10", {
+            block:
+              openDropdownUser ||
+              openDropdownNotifications ||
+              openDropdownApps ||
+              openDropdownSearch,
+            hidden:
+              !openDropdownUser &&
+              !openDropdownNotifications &&
+              !openDropdownApps &&
+              !openDropdownSearch,
+          })
+        )}
+        onClick={() => {
+          setOpenDropdownUser(false);
+          setOpenDropdownNotifications(false);
+          setOpenDropdownApps(false);
+          setOpenDropdownSearch(false);
+        }}
+      ></div>
+      <div className="navbar flex justify-end items-center gap-2">
+        <button
+          className="flex items-center w-8 h-8"
+          type="button"
+          onClick={() => setOpenDropdownSearch(!openDropdownSearch)}
+        >
+          <RiSearchLine />
+        </button>
+        <button
+          className="flex items-center w-8 h-8"
+          type="button"
+          onClick={() => setOpenDropdownApps(!openDropdownApps)}
+        >
+          <RiApps2Fill />
+        </button>
+        <button
+          className="flex items-center w-8 h-8"
+          type="button"
+          onClick={() =>
+            setOpenDropdownNotifications(!openDropdownNotifications)
+          }
+        >
+          <RiNotification3Fill />
+        </button>
+        <button
+          className="flex items-center w-8 h-8"
+          type="button"
+          onClick={() => setOpenDropdownUser(!openDropdownUser)}
+        >
+          <RiUser3Fill />
+          <RiArrowDownSLine />
+        </button>
       </div>
+      <HeaderDropdown
+        menu={menuSearch}
+        openDropdown={openDropdownSearch}
+        nivel={4}
+      />
+      <HeaderDropdown
+        menu={menuApps}
+        openDropdown={openDropdownApps}
+        nivel={3}
+      />
+      <HeaderDropdown
+        menu={menuNotifications}
+        openDropdown={openDropdownNotifications}
+        nivel={2}
+      />
+      <HeaderDropdown
+        menu={menuUser}
+        openDropdown={openDropdownUser}
+        nivel={1}
+      />
     </div>
   );
 }
