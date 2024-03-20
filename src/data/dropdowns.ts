6e3c54cfa70c235e12742944f701bdd01269b5d1
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-03-20 13:53:39 -0500

    Add dropdown menu data

---
+++ src/data/dropdowns.ts
@@ -0,0 +1,116 @@
+export const menuSearch = [
+  {
+    id: 1,
+    name: "Buscar",
+    icon: "",
+    path: "/#buscar",
+    active: false,
+  },
+];
+
+export const menuApps = [
+  {
+    id: 1,
+    name: "Instagram",
+    icon: "",
+    path: "/#instagram",
+    active: false,
+  },
+  {
+    id: 2,
+    name: "Facebook",
+    icon: "",
+    path: "/#facebook",
+    active: false,
+  },
+  {
+    id: 3,
+    name: "Twitter",
+    icon: "",
+    path: "/#twitter",
+    active: false,
+  },
+  {
+    id: 4,
+    name: "Linkedin",
+    icon: "",
+    path: "/#linkedin",
+    active: false,
+  },
+  {
+    id: 5,
+    name: "Youtube",
+    icon: "",
+    path: "/#youtube",
+    active: false,
+  },
+  {
+    id: 6,
+    name: "Whatsapp",
+    icon: "",
+    path: "/#whatsapp",
+    active: false,
+  },
+];
+
+export const menuNotifications = [
+  {
+    id: 1,
+    name: "Mensajes",
+    icon: "",
+    path: "/#mensajes",
+    active: false,
+  },
+  {
+    id: 2,
+    name: "Correo",
+    icon: "",
+    path: "/#correo",
+    active: false,
+  },
+  {
+    id: 3,
+    name: "Reacciones",
+    icon: "",
+    path: "/#reacciones",
+    active: false,
+  },
+];
+
+export const menuUser = [
+  {
+    id: 1,
+    name: "Perfil",
+    icon: "",
+    path: "/#perfil",
+    active: false,
+  },
+  {
+    id: 2,
+    name: "Configuración",
+    icon: "",
+    path: "/#configuracion",
+    active: false,
+  },
+  {
+    id: 3,
+    name: "Cambio Pass",
+    icon: "",
+    path: "/#cambio-pass",
+    active: false,
+  },
+  {
+    id: 4,
+    name: "Ayuda",
+    icon: "",
+    path: "/#ayuda",
+    active: false,
+  },
+  {
+    id: 5,
+    name: "Cerrar sesión",
+    icon: "",
+    path: "/#cerrar-sesion",
+    active: false,
+  },
+];
