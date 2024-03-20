6f3935fdb6b0201909fea6f4ac69c93118f56f92
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-03-19 20:21:41 -0500

    Update TabButton border width

--- src/components/ui/TabButton.tsx
+++ src/components/ui/TabButton.tsx
@@ -10,7 +10,7 @@ export function TabButton({ children, active, ...props }: Props) {
     <button
       className={twMerge(
         clsx(
-          "text-[#808997] border-transparent flex-1 whitespace-nowrap hover:bg-slate-300 border-b-2 px-2 py-2 text-base font-medium border-indigo-500 leading-6",
+          "text-[#808997] border-transparent flex-1 whitespace-nowrap hover:bg-slate-300 border-b-4 px-2 py-2 text-base font-medium border-indigo-500 leading-6",
           { "border-indigo-500": active },
           { "border-transparent ": !active }
         )
