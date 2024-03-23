2c2d802e11868d554d4e70fbb07ec363c82c5bad
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-03-23 14:22:12 -0500

    Add menu data to menu.ts

---
+++ src/data/menu.ts
@@ -0,0 +1,37 @@
+export const menu = [
+  {
+    id: 1,
+    name: "Inicio",
+    icon: "",
+    path: "/",
+    active: false,
+  },
+  {
+    id: 2,
+    name: "Proyectos",
+    icon: "",
+    path: "/#projects",
+    active: true,
+  },
+  {
+    id: 3,
+    name: "Planeación",
+    icon: "",
+    path: "/#planning",
+    active: false,
+  },
+  {
+    id: 4,
+    name: "Equipo",
+    icon: "",
+    path: "/#team",
+    active: false,
+  },
+  {
+    id: 5,
+    name: "Calendario",
+    icon: "",
+    path: "/#calendar",
+    active: false,
+  },
+];
